a43045136c47a80aad26625afb04cc7b2517ba2e
Random01 <random01@rambler.ru>
2021-10-23 23:46:23 +0400

    - other changes

--- firmware/weather_station/weather_station.ino
+++ firmware/weather_station/weather_station.ino
@@ -11,18 +11,28 @@
 
 #include <Wire.h>
 #include <SPI.h>
+
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <RTClib.h>
 
-#include "GyverTM1637.h"
-#include "GyverEncoder.h"
+#include <TimerOne.h>
+#include <TM74HC595Display.h>
 
-#define DISPLAY_CLK   5 // DISPLAY pins definitions for TM1637 and can be changed to other ports
-#define DISPLAY_DIO   4 // DISPLAY pins definitions for TM1637 and can be changed to other ports
+#define SCLK  4 // TM74HC595 Pin
+#define RCLK  3 // TM74HC595 Pin
+#define DIO   2 // TM74HC595 Pin
+
+#define FIRST_DIGIT   3
+#define SECOND_DIGIT  2
+#define THIRD_DIGIT   1
+#define FOURTH_DIGIT  0
+#define POINT_SHIFT   10
+
+#define EMPTY_SYMBOL  0b11111111 // " "
+#define DASH_SYMBOL   0b10111111 // "-"
 
 #define ONE_WIRE_BUS  10 // DS1820 Pin
 
@@ -37,65 +47,30 @@
 #define PRESSURE_MODE        2
 #define HUMIDITY_MODE        3
 #define DEW_POINT            4
-#define TIME_MODE            5
-#define SETTINGS_MODE        6
 
 #define MENU_ITEMS_COUNT 7
 
+// If you see "Could not find a valid BME280 sensor, check wiring!"
 // redefine BME280_ADDRESS to 0x76 in Adafruit_BME280.h
 Adafruit_BME280 bme; // I2C
-
-GyverTM1637 disp(DISPLAY_CLK, DISPLAY_DIO);
-
-Encoder enc1(ENCODER_CLK, ENCODER_DT, ENCODER_SW);
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensor(&oneWire);
 
-RTC_DS3231 rtc;
+TM74HC595Display disp(SCLK, RCLK, DIO);
 
-int mode = TIME_MODE;
+unsigned char SYMBOL[20];
 
-int previous_second = -1;
+int mode = TEMPERATURE_BME_MODE;
+
 bool is_updating = false;
-
-bool curr_dots = POINT_OFF; // displays dots in clock
 
 void setup() {
   Serial.begin(9600);
 
   initBmeSensor();
-  initRTC();
   initDs1Sensor();
   initDisplay();
-  initEncoder();
-}
-
-void initEncoder() {
-  enc1.setType(TYPE2);
-}
-
-void initRTC() {
-  Serial.println(F("RTC test"));
-
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find a valid RTC, check wiring!");
-    while (1);
-  }
-
-  Serial.println("RTC has been found");
-  Serial.println();
-}
-
-void restoreTime() {
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, lets set the time!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
 }
 
 void initBmeSensor() {
@@ -115,61 +90,46 @@ void initDs1Sensor() {
   sensor.setResolution(12);
 }
 
+void initDisplay() {
+  symbols();                         // объявление пачки символов для работы дисплея
+
+  Timer1.initialize(1500);           // установка таймера на каждые 1500 микросекунд
+  Timer1.attachInterrupt(timerIsr);  // запуск таймера
+}
+
 void loop() {
-  tickEncoder();
+  is_updating = true;
 
-  DateTime now = rtc.now();
-  uint8_t second =  now.second();
-  if (previous_second != second) {
-    previous_second = second;
-    curr_dots = !curr_dots;
-    // let's update UI each second
-    is_updating = true;
-  }
-
+  mode = TEMPERATURE_BME_MODE;
   tickTemperatureBme();
+
+  delay(2000);
+
+  mode = TEMPERATURE_DS1_MODE;
   tickTemperatureDs1();
 
+  delay(2000);
+
+  mode = PRESSURE_MODE;
   tickPressure();
+
+  delay(2000);
+
+  mode = HUMIDITY_MODE;
   tickHumidity();
+
+  delay(2000);
+
+  mode = DEW_POINT;
   tickDewPoint();
 
-  tickTime();
-
-  tickSettings();
-
-  is_updating = false;
-}
-
-void initDisplay() {
-  disp.clear();
-  disp.brightness(BRIGHT_TYPICAL);
-}
-
-void tickEncoder() {
-  enc1.tick();
-
-  if (enc1.isRight()) mode++;
-  if (enc1.isLeft()) mode--;
-
-  if (mode < 0) {
-    mode = MENU_ITEMS_COUNT - 1;
-  } else if (mode >= MENU_ITEMS_COUNT) {
-    mode = 0;
-  }
-
-  if (enc1.isTurn()) {
-    is_updating = true;
-
-    disp.point(POINT_OFF);
-    disp.displayByte(0x40, 0x40, 0x40, 0x40);
-  }
+  delay(2000);
 }
 
 void tickTemperatureBme() {
   if (is_updating == true && TEMPERATURE_BME_MODE == mode) {
     float temperature = bme.readTemperature(); // *C
-    printTemperature(temperature);
+    displayTemperature(temperature);
   }
 }
 
@@ -177,14 +137,14 @@ void tickTemperatureDs1() {
   if (is_updating == true && TEMPERATURE_DS1_MODE == mode) {
     sensor.requestTemperatures();
     float temperature = sensor.getTempCByIndex(0); // *C
-    printTemperature(temperature);
+    displayTemperature(temperature);
   }
 }
 
 void tickPressure() {
   if (is_updating == true && PRESSURE_MODE == mode) {
-    float pressure = bme.readPressure() / 100.0F; // hPa
-    printHumidity(pressure);
+    float pressure = bme.readPressure(); // hPa
+    printPressure(pressure);
   }
 }
 
@@ -195,30 +155,17 @@ void tickHumidity() {
   }
 }
 
-void tickTime() {
-  if (is_updating && TIME_MODE == mode) {
-    DateTime now = rtc.now();
-    printTime(now.hour(), now.minute(), curr_dots);
-  }
-}
-
-void tickSettings() {
-  if (SETTINGS_MODE == mode) {
-    // Proccess available settings
-  }
-}
-
 void tickDewPoint() {
   if (is_updating && DEW_POINT == mode) {
     float dewPoint = getDewPoint();
-    printTemperature(dewPoint);
+    displayTemperature(dewPoint);
   }
 }
 
 /**
- * Calculates Dew Point.
- * https://en.wikipedia.org/wiki/Dew_point#Calculating_the_dew_point
- */
+   Calculates Dew Point.
+   https://en.wikipedia.org/wiki/Dew_point#Calculating_the_dew_point
+*/
 float getDewPoint() {
   float b = 17.62;
   float c = 243.12;
@@ -230,31 +177,84 @@ float getDewPoint() {
 }
 
 /**
- * "29:8_" <- 28.9C
- * "_9:5_" <- 9.5C
- * "_9:5-" <- -9.5C
- */
-void printTemperature(float temperature) {
-  disp.displayInt(int(temperature));
-}
-
-/**
- * 49.7p
- */
-void printHumidity(float humidity) {
-  disp.displayInt(int(humidity));
-}
-
-/**
- * pressure in "hPa"
- * Displays pressure in "mm Hg".
- */
+   pressure in "hPa"
+   Displays pressure in "mm Hg".
+*/
 void printPressure(float pressure) {
   pressure = pressure / 133.322;
-  disp.displayInt(int(pressure));
+
+  int firstDigit = (int) pressure / 100; // 768 -> 7
+  int secondDigit =  (int)(pressure - firstDigit * 100) / 10; // 768 -> 6
+  int thirdDigit = (int) pressure % 10;  // 768 -> 8
+
+  disp.set(SYMBOL[firstDigit], FIRST_DIGIT);
+  disp.set(SYMBOL[secondDigit], SECOND_DIGIT);
+  disp.set(SYMBOL[thirdDigit], THIRD_DIGIT);
+  disp.set(_P, FOURTH_DIGIT);
 }
 
-void printTime(uint8_t hours, uint8_t minutes, bool dots) {
-  disp.point(dots);
-  disp.displayClock(hours, minutes);
+/**
+   Humidity in %
+   Displays humidity in %
+   67.7 -> 67H
+*/
+void printHumidity(float humidity) {
+  int firstDigit = (int) humidity / 100; // 100 -> 1
+  int secondDigit = (int) humidity / 10; // 98 -> 9
+  int thirdDigit = (int) humidity % 10;  // 98 -> 8
+
+  disp.set((firstDigit > 0) ? SYMBOL[firstDigit] : EMPTY_SYMBOL, FIRST_DIGIT);
+  disp.set((secondDigit > 0) ? SYMBOL[secondDigit] : EMPTY_SYMBOL, SECOND_DIGIT);
+  disp.set(SYMBOL[thirdDigit], THIRD_DIGIT);
+  disp.set(_H, FOURTH_DIGIT);
+}
+
+void displayTemperature(float temperature) {
+  int sign = temperature < 0;
+  temperature = abs(temperature);
+
+  int firstDigit = (int) temperature / 10; // 25.8 -> 2
+  int secondDigit = (int) temperature % 10;  // 25.8 -> 5
+  int fractionDigit = (int) ((temperature  - (firstDigit * 10 + secondDigit)) * 10); // 25.8 -> 8
+
+  disp.set((sign && firstDigit > 0) ? DASH_SYMBOL : EMPTY_SYMBOL, FIRST_DIGIT);
+
+  if (firstDigit == 0) {
+    disp.set(sign ? DASH_SYMBOL : EMPTY_SYMBOL, SECOND_DIGIT);
+  } else {
+    disp.set(SYMBOL[firstDigit], SECOND_DIGIT);
+  }
+
+  disp.set(SYMBOL[POINT_SHIFT + secondDigit], THIRD_DIGIT);
+  disp.set(SYMBOL[fractionDigit], FOURTH_DIGIT);
+}
+
+void timerIsr() {   // прерывание таймера
+  disp.timerIsr();  // пнуть дисплей
+}
+
+void symbols() {
+  // обычные
+  SYMBOL[0] = 0xC0; //0
+  SYMBOL[1] = 0xF9; //1
+  SYMBOL[2] = 0xA4; //2
+  SYMBOL[3] = 0xB0; //3
+  SYMBOL[4] = 0x99; //4
+  SYMBOL[5] = 0x92; //5
+  SYMBOL[6] = 0x82; //6
+  SYMBOL[7] = 0xF8; //7
+  SYMBOL[8] = 0x80; //8
+  SYMBOL[9] = 0x90; //9
+
+  // с точкой
+  SYMBOL[10] = 0b01000000; //0.
+  SYMBOL[11] = 0b01111001; //1.
+  SYMBOL[12] = 0b00100100; //2.
+  SYMBOL[13] = 0b00110000; //3.
+  SYMBOL[14] = 0b00011001; //4.
+  SYMBOL[15] = 0b00010010; //5.
+  SYMBOL[16] = 0b00000010; //6.
+  SYMBOL[17] = 0b01111000; //7.
+  SYMBOL[18] = 0b00000000; //8.
+  SYMBOL[19] = 0b00010000; //9.
 }
